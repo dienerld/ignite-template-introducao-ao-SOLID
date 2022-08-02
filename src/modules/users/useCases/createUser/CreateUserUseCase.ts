d891249dd39f6cf55d7f913fb3008dca34fe79e1
Diener Dornelas <diener.ld@outlook.com>
2022-08-02 09:31:38 -0300

    finish challenge

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,12 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const alreadyExists = this.usersRepository.findByEmail(email);
+    if (alreadyExists) {
+      throw new Error("Mensagem do erro");
+    }
+    const user = this.usersRepository.create({ name, email });
+    return user;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,22 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.headers.user_id as string;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      if (users.length <= 0) {
+        return response.status(404).json({
+          error: "No users found",
+        });
+      }
+      return response.status(200).json(users);
+    } catch (err) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
   }
 }
 
