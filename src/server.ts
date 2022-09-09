e81203a326e44837f1a786de75a97498ddbf0e16
Diener Dornelas <dienezim@hotmail.com>
2022-09-09 11:13:55 -0300

    add swagger doc

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import swaggerUI from "swagger-ui-express";
+
 import { app } from ".";
+import docs from "./docs/swagger.json";
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(docs));
 
 app.listen(3333, () => console.log("Server is running!"));
